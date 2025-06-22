154c15ac2f587928240880f6141e6b05202d58ec
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-22 09:21:00 +0530

    add navigation component and implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Product from './components/Product'
-import About from './components/About'
-import Services from './components/Services'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import About from "./components/About";
+import Services from "./components/Services";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-800'>
+    <div className="w-full h-screen text-white bg-zinc-800">
+      <Nav />
       <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/product' element = {<Product />} />
-        <Route path='/services' element = {<Services />} />
-        <Route path='/about' element = {<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
